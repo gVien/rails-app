4af555708cf74a6dc7bc3b718ca4189b6a305fbe
gVien <wavetech86@gmail.com>
2015-09-19 19:49:54 -0700

    add integration test & minor update to header root path

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template "static_pages/home"
+    # the question mark is replaced by the path url inside a[...]
+    # e.g. <a href="/about"> ... </a>
+    # for the root path, it verifies two links (one for the logo and navigation menu element)
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?", help_path
+    assert_select "a[href=?", about_path
+    assert_select "a[href=?", contact_path
+  end
+end
