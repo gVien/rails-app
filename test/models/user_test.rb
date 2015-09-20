f2ee347a83ac818a55fd2e828b7824d25bdd975d
gVien <wavetech86@gmail.com>
2015-09-20 12:16:33 -0700

    add test and validation for name and email for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # this method automatically gets run before each test
+  def setup
+    @user = User.new(name: "Test User", email: "user@example.com")
+  end
+
+  # is this a valid user? test
+  # asset method succeeds if valid? method returns true, fails if it's false
+  test "should be valid" do
+    assert(@user.valid?)
+  end
+
+  # assert_not(object, message=nil)
+  # returns true if object is nil or false
+  # returns the error message if it is false (default to nil, which is falsy)
+  # if object is true, then it's truthy
+  test "should be invalid" do
+    # blank name is considered not present in model
+    @user.name = "   "
+    # since @user.valid? is false, this returns true
+    assert_not(@user.valid?)
+  end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not(@user.valid?)
+  end
 end
