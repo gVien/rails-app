7d84b8aabb0b44df7dc43aaea35c979f8f1de948
gVien <wavetech86@gmail.com>
2015-09-23 15:32:36 -0700

    add integration test for valid signup information

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,4 +19,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template "users/new"
   end
+
+  # testing if submission is valid (created) and confirm the contents of the database
+  test "valid signup information" do
+    get signup_path
+    # works similar to assert_no_difference
+    # but in this case, we want to see a differene of 1 (that parameter is optinal)
+    assert_difference "User.count", 1 do
+      # post_via_redirect is used to redirect after post, resulting in rendering of "users/show" template
+      post_via_redirect users_path, user: { name: "Gai",
+        email: "gai@gai.com",
+        password: "password",
+        password_confirmation: "password"}
+    end
+    assert_template "users/show"
+  end
 end
