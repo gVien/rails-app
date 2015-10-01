ffb767efed55e80472a141965d968137d1918c60
gVien <wavetech86@gmail.com>
2015-09-30 22:18:53 -0700

    update user fixture & add login integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :length => { maximum: 255 }, :format => { with: VALID_EMAIL_REGEX }, :uniqueness => { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Now time to test the login functionality of the site (8.2.4).
+  # the flow will be the following:
+  # 1. Visit the login path.
+  # 2. Post valid information to the sessions path.
+  # 3. Verify that the login link disappears.
+  # 4. Verify that a logout link appears
+  # 5. Verify that a profile link appears.
+  # in order to do this test, we need to test up BCrypt for the test
+  # THIS WILL BE USED WITH THE FIXTURES IN TEST
+  # We can create a user fixture with this new method
+  def self.digest(string)
+    # https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
+    # source from the above link
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    # string is the string that needs to be hash and cost is the computational cost to calculate the hash. The higher the cost, it will be harder to determine the original password
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  # define setup (this runs when the test is loaded) for the login test
+  def setup
+    @user = users(:gai)
+  end
+
   # add integration test for invalid login information
   # the process is listed below
   # 1. Visit the login path.
@@ -18,5 +23,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty? # test only pass if flash.empty is false. # 4
     get root_path # #5
     assert flash.empty?  # #6
+  end
+
+  # see User model for more description but
+  # the flow will be the following:
+  # 1. Visit the login path.
+  # 2. Post valid information to the sessions path.
+  # 3. Verify that the login link disappears.
+  # 4. Verify that a logout link appears
+  # 5. Verify that a profile link appears.
+  # type this to test the file
+  # bundle exec rake test TEST=test/integration/users_login_test.rb
+  # can add 'TESTOPTS="--name test_login_with_valid_information" to test only the test below'
+  test "login with valid information" do
+    get login_path  # 1
+    post login_path, session: { email: @user.email, password: "123456" }  #2
+    assert_redirected_to @user  #2 (assert_redirected_to checks the right redirect target)
+    follow_redirect!    #2 (this visits the target page)
+    assert_template("users/show")   #2
+    assert_select "a[href=?]", login_path, count: 0   #3 (this test if the login_path url has a count of 0, meaning it's no longer there)
+    assert_select "a[href=?]", logout_path  #4 (this test for logout url is there in the HTML)
+    assert_select "a[href=?]", user_path(@user)   #5 (test if user, e.g. /users/1 is there)
   end
 end
