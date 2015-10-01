0fca5486bd6424033ed1ee5202af404a9a361c0a
gVien <wavetech86@gmail.com>
2015-10-01 14:56:05 -0700

    clarify the parameter in  authenticated? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,7 @@ class User < ActiveRecord::Base
   # returns true if the given token matches the digest
   # this is similar to authenticate method from BCrypt
   def authenticated?(remember_token)
+    # remember_token is not the same as the accessor (this is a reference, in case there is a confusion in the future)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)  # note this is the same as BCrypt::Password.new(remember_digest).is_password?(remember_token) but it is clearer
   end
 end
