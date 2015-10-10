cfc70898de936dedf97ee6bd634f1e5e1f6630eb
gVien <wavetech86@gmail.com>
2015-10-10 15:59:16 -0700

    update password reset ctrler to apply expiration & update action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,10 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: [:edit, :update] #checks if the user exists before the edit & update action can be performed
-  before_action :valid_user, only: [:edit, :update] #check if user is valid before  the edit & update action can be performed (it renders password reset page)
+  #checks if the user exists before the edit & update action can be performed
+  before_action :get_user, only: [:edit, :update]
+  #check if user is valid before  the edit & update action can be performed (it renders password reset page)
+  before_action :valid_user, only: [:edit, :update]
+  # check for expiration before the edit & update action can be performed
+  before_action :check_expiration, only: [:edit, :update] # case #1
 
   def new
   end
@@ -22,10 +26,36 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  # edit & update check for 4 cases
+  # 1. an expired reset (applies to edit & update)
+  # 2. a successful reset (applies to update)
+  # 3. a failed reset (invalid password) - applies to update
+  # 4. a failed reset that seems successful initially (a blank password) - applies to update
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?  # case #4
+      # add error for password if it's empty
+      @user.errors.add(:password, "Password cannot be empty")
+      render "edit"
+    elsif @user.update_attributes(user_params) # case #2
+      log_in @user
+      flash[:success] = "Password reset successfully"
+      redirect_to @user
+    else # case #3
+      render "edit"
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # before filters
+
     # find the user (in the password reset edit view)
     def get_user
       @user = User.find_by(email: params[:email])
@@ -36,6 +66,14 @@ class PasswordResetsController < ApplicationController
       unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])  #note params[:id] is the token (and not the numerical id, e.g. 1, 5, etc)
         flash[:info] = "Invalid Request."
         redirect_to root_url
+      end
+    end
+
+    # check if password is expired
+    def check_expiration
+      if @user.password_reset_expired?  # need to define this method in User ctrler
+        flash[:danger] = "The password reset link has expired. Please reset your password again"
+        redirect_to new_password_reset_url
       end
     end
 end
