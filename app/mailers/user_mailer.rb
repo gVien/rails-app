2022efd51ddffa152358485b4da7ef50f699ed8d
gVien <wavetech86@gmail.com>
2015-10-10 10:43:23 -0700

    add reset password views, preview of email, password_reset method, to pass tests

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ApplicationMailer
 
+  # sends activation email
   def account_activation(user)
     @user = user  # instance variable so the mailer view can have access
     mail to: user.email, subject: "Account Activation"  # sending the email to this user
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # sends password reset instructions email
+  def password_reset(user)
+    @user = user  # instance variable so the mailer view can have access
+    mail to: @user.email, subject: "Password Reset"
   end
 end
