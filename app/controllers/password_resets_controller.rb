bf7c95c8f5e5393cf6870d47d2ab3dcb0dffd09a
gVien <wavetech86@gmail.com>
2015-10-10 14:21:31 -0700

    add methods to password reset cntrler to check for existence & validity for user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,11 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update] #checks if the user exists before the edit & update action can be performed
+  before_action :valid_user, only: [:edit, :update] #check if user is valid before  the edit & update action can be performed (it renders password reset page)
+
   def new
   end
 
+  # generate password reset info (token + digest + send email) after request
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
@@ -20,4 +24,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+    # find the user (in the password reset edit view)
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # verify if the user is valid, activated, and authenticated (if a user has submitted password reset form) otherwise redirect to root url
+    def valid_user
+      unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])  #note params[:id] is the token (and not the numerical id, e.g. 1, 5, etc)
+        flash[:info] = "Invalid Request."
+        redirect_to root_url
+      end
+    end
 end
