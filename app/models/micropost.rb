334d651e8575f879250da4d85020b943ebbc9bd1
gVien <wavetech86@gmail.com>
2015-10-11 00:43:52 -0700

    add default scope to reorder microposts, pass model test

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-
+  # -> is “stabby lambda” syntax for an object called a Proc (procedure) or lambda
+  default_scope -> { order(created_at: :desc) } # create default order for microposts
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140} # or { in: 1..140 }
 end
