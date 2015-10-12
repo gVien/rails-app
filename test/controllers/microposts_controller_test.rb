b804a3899c90dc65aa5d52e4c62d701255015d03
gVien <wavetech86@gmail.com>
2015-10-11 20:52:33 -0700

    add TDD for microposts controller & routes for microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @micropost = microposts(:most_recent)
+  end
+
+  # 1. verify the count does not change when the user attempts to crate a micropost
+  # 2. verify it redirects to login page
+  test "should redirect create if user is not logged in" do
+    assert_no_difference "Micropost.count" do
+      post(:create, micropost: { content: "illegal micropost cannot be created "})
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy if user is not logged in" do
+    assert_no_difference "Micropost.count" do
+      delete(:destroy, id: @micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
