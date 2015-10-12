c8d6198f9b990e79bfe4f5ca797bfce74b661b98
gVien <wavetech86@gmail.com>
2015-10-12 08:25:22 -0700

    add micropost forms, update micropost ctrler for create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,8 +3,20 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost successfully created!"
+      redirect_to root_url
+    else
+      render "static_pages/home"  # root_url
+    end
   end
 
   def destroy
   end
+
+  private
+    def micropost_params
+      params(:micropost).permit(:content)
+    end
 end
