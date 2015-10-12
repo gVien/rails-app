8a06f927f1a30442efdd4089dca9ec26e11e1df5
gVien <wavetech86@gmail.com>
2015-10-11 18:44:54 -0700

    add microposts seed, fix typo in file name, add styling to microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,9 @@ User.create!( name:  "Example User",
                 activated: true,
                 activated_at: Time.zone.now )
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(10)
+  users.each { |user| user.microposts.create!(content: content) }
+end
