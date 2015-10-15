96de5a9bb6d8baf7659d4a09a8ac94ddb1bc6418
gVien <wavetech86@gmail.com>
2015-10-14 23:49:12 -0700

    add microposts sidebar count integration test (ch11,ex2)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -45,4 +45,30 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(@new_user)
     assert_select "a", text: "delete", count: 0 # delete link is not there
   end
+
+  # ch11, ex 2: testing the sidebar count
+  # login as user
+  # get root_path
+  # verify the count number is in the body
+  # login as another user with zero micropost
+  # get root path
+  # verify the 0 count number is in the body
+  # create a post
+  # get the root path again
+  # verify the new count matches with the body (this also verifies the pluralization of "micropost")
+  test "microposts sidebar count" do
+    # count above 0
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # count of 0
+    kathy = users(:kathy)
+    log_in_as(kathy)
+    get root_path
+    assert_match "0 microposts", response.body
+    # create a post and check
+    kathy.microposts.create!(content: "new post")
+    get root_path
+    assert_match "#{kathy.microposts.count} micropost", response.body
+  end
 end
