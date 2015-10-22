6559912a1a5c74297c916b89b501f1c437bd51b3
gVien <wavetech86@gmail.com>
2015-10-22 11:40:05 -0700

    add ajax for follow/unfollow button & stat count of followers

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,18 +3,27 @@ class RelationshipsController < ApplicationController
 
   def create
     # get the id of the user that the current user wants to follow in the hidden field (see follow form)
-    user = User.find(params[:followed_id])
+    # instance variable is needed for ajax call for the follow/unfollow button
+    @user = User.find(params[:followed_id])
     #current user follows the user
-    current_user.follow(user)
-    #redirect back to the user page (to refresh the page)
-    redirect_to user_path(user) # or simply user
+    current_user.follow(@user)
+    respond_to do |format|
+      #redirect back to the user page (to refresh the page)
+      format.html { redirect_to user_path(@user) } # or simply @user
+      # for ajax
+      format.js
+    end
   end
 
   def destroy
     # find the user that the current user is following
     #followed is the user that athe current user is following (note there is also `follower` which is the current user)
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    # instance variable is needed for ajax call for the follow/unfollow button
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
