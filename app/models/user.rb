4f3bb5dd31915d57524a7706f9e2287e1130d2f8
gVien <wavetech86@gmail.com>
2015-10-22 19:02:15 -0700

    refactor code status feed method for scalability

--- app/models/user.rb
+++ app/models/user.rb
@@ -113,7 +113,9 @@ class User < ActiveRecord::Base
     # question mark ensures the id is properly escaped before being included in the underlying SQL query.
     # this will avoid a serious security known as the SQL injection
     # id is an integer, to prevent SQL injection but escaping is a good practice
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id) # update feed for status feed implementation
+    # following_ids is an array of all the user id of the list current.following
+    # this refactor is more convenient since we can use the variable inserted in more than one place
+    Micropost.where("user_id IN (:following_ids) OR user_id = ?", following_ids: following_ids, user_id: id) # update feed for status feed implementation
   end
 
   # method to follow a user, e.g. gai.follow(kathy) => gai follows kathy
